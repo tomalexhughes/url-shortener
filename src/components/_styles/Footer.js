bcb84d4a8658ba79b86a0f07abffc279af4a3850
Tom Hughes <tom@motordelivery.co.uk>
2019-07-12 18:56:56 +0100

    Swaps footer position from left to right

--- src/components/_styles/Footer.js
+++ src/components/_styles/Footer.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Footer = styled.footer`
     position: absolute;
     bottom: 2rem;
-    left: 2rem;
+    right: 2rem;
     color: white;
     font-weight: 100;
     font-size: 1.6rem;
