08ac44bd880c900ce5907c1a11a9e39d06137b15
Tom Hughes <tom+git@tah.dev>
2023-06-15 12:32:07 +0100

    build: updates to React 18
    NOTE: This commit does not include updates to `@testing-library/react`.
    Tests will still pass, but will use the old `ReactDOM.render` API, and
    log warnings.

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from '.';
 
 describe('App', () => {
     it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<App />, div);
-        ReactDOM.unmountComponentAtNode(div);
+        const container = document.createElement('div');
+        const root = createRoot(container);
+        root.render(<App />);
+        root.unmount();
     });
 });
